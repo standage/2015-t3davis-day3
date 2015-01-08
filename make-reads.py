50b29542d6aefe1c17a6a5d161c0ef7c59f5d39c
Daniel Standage <daniel.standage@gmail.com>
2015-01-08 10:27:24 -0800

    Argparse

--- make-reads.py
+++ make-reads.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 import screed
 import sys
@@ -5,17 +6,31 @@ import sys
 N = 1000
 L = 100
 
-genomefile = sys.argv[1]
-for record in screed.open(genomefile):
-  genome = record.sequence
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("genomefile")
+  parser.add_argument("--error", type=int, default=L)
 
-for i in range(N):
-  start = random.randint(0, len(genome) - L - 1)
-  end = start + L
-  read = genome[start:end]
+  genomefile = sys.argv[1]
+  for record in screed.open(genomefile):
+    genome = record.sequence
 
-  if random.choice([0,1]):
-    read = screed.rc(read)
+  for i in range(N):
+    start = random.randint(0, len(genome) - L - 1)
+    end = start + L
+    read = genome[start:end]
 
-  assert len(read) == L
-  print '>read%d\n%s' % (i, read)
+    for _ in range(L):
+      if random.randint(0, L - 1) == 0:
+        position = random.randint(0, L-1)
+        newbase = random.choice(["a", "c", "g", "t"])
+        read = read[:position] + newbase + read[position+1:]
+
+    if random.choice([0,1]):
+      read = screed.rc(read)
+
+    assert len(read) == L
+    print '>read%d\n%s' % (i, read)
+
+if __name__ == "__main__":
+  main()
